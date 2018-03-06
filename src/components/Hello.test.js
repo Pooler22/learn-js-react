aabece23c89cd5a47a004a29c86cba67858a91f6
pawelrutkowski <rutkowskip22@gmail.com>
2018-03-06 06:13:45 +0100

    addded tests js

--- src/components/Hello.test.js
+++ src/components/Hello.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import Hello from "./Hello";
 
-describe("Hello component", () => {
-  const wrapper = shallow(<Hello name="name" />);
+test("Hello component", () => {
+  const wrapper = shallow(<Hello name={"name"} />);
 
   expect(wrapper).toMatchSnapshot();
 });
